12f8544144f735bd0656123f91d9bde8cf621739
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-02 07:47:37 -0700

    Agregar archivos de proyecto.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void btnJaja_Clicked(object sender, EventArgs e)
+        {
+            float cantidad = float.Parse(txtCantidad.Text);
+            float diezPorciento = cantidad * .1f;
+            lblResultado.Text = diezPorciento.ToString();
+        }
+    }
+}
